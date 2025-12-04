7135e75f728aa236fb488dbc99184b7b01d81581
Pupkin512 <pupkin123@tutamail.com>
2025-12-04 23:40:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+    import Euclid
+    import Pascal
+    import exchanges
+    import perfection
+ 
+    if __name__ == '__main__':
+        Euclid.main()  
+        Pascal.main()
+        exchanges.main()
+        perfection.main() 
+        
+
+
+
